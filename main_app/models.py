d7f7775554eebc8478cbaf5698c07f429beeefac
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-05-14 10:08:33 -0700

    Added db to settings.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import Model, CharField, BooleanField, TextField, DateTimeField
 
 # Create your models here.
+
+class User(Model):
+
+    name = CharField(max_length=100)
+    currentLocation = CharField(max_length=500)
+    join_date = DateTimeField(auto_now_add=True)
