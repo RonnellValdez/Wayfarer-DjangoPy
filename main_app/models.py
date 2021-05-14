6e6a881f8ef8727abaf26bb627c3e00455dd9b14
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-05-14 12:02:51 -0700

    seed file created, seattle populates city collection

--- main_app/models.py
+++ main_app/models.py
@@ -9,3 +9,9 @@ class Profile(Model):
     current_city = CharField(max_length=50)
     current_country = CharField(max_length=50)
     join_date = DateTimeField(auto_now_add=True)
+
+class City(Model):
+    name = CharField(max_length=50)
+    country = CharField(max_length=50)
+    image_thumbnail = CharField(max_length=500)
+    image_detailed = CharField(max_length=500)
