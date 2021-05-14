d02bdc5377a80fb34165bf7cc73eaf9f417bff64
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-05-14 11:21:15 -0700

    profile class created

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
-from django.db.models import Model, CharField, BooleanField, TextField, DateTimeField
-
+from django.db.models import Model, CharField, BooleanField, TextField, DateTimeField, OneToOneField
+from django.contrib.auth.models import User
 # Create your models here.
 
-class User(Model):
 
-    name = CharField(max_length=100)
-    currentLocation = CharField(max_length=500)
+class Profile(Model):
+    user = OneToOneField(User, on_delete=models.CASCADE)
+    current_city = CharField(max_length=50)
+    current_country = CharField(max_length=50)
     join_date = DateTimeField(auto_now_add=True)
