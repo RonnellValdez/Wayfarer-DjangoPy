337d91c2ad12c109ed2bd7f39e762b547f377e04
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-05-15 12:54:56 -0700

    New users now have a Profile, still tweaking the sign up page to display all fields for signing up

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.db.models import Model, CharField, BooleanField, TextField, DateTimeField, OneToOneField, ForeignKey
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
@@ -12,6 +14,19 @@ class Profile(Model):
 
     def __str__(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
+
+        
+    
 
 class City(Model):
     name = CharField(max_length=50)
