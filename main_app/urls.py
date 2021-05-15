28e6e510e82c00df6546c2231e5973cba0c701ab
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-05-15 08:34:09 -0700

    Added a signup html and url path.  Created a Signup view.  Added LOGOUT_REDIRECT_URL to settings.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('profile', views.Profile.as_view(), name="profile"),
+    path('signup/', views.Signup.as_view(), name="signup")
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.base import TemplateView
 
 # Create your views here.
@@ -9,3 +11,20 @@ class Home(TemplateView):
 
 class Profile(TemplateView):
     template_name = 'profile.html'
+
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {'form': form}
+        return render(request, "signup.html", context)
+
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            context = {"form": form}
+            return render(request, "signup.html", context)
