70c061d73456ab6ea4f30b1a2093f448c4ee1e72
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-05-15 15:34:49 -0700

    Removed unecessary imports from the forms.py file.  Now the fields show up on signup.html

--- main_app/forms.py
+++ main_app/forms.py
@@ -4,11 +4,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
+    current_city = forms.CharField(max_length=50)
+    current_country = forms.CharField(max_length=50)
 
-    class Meta:
+    class Meta():
         model = User
         fields = ('username', 'first_name', 'last_name')
     
