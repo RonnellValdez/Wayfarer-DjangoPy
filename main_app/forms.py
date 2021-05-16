29cabe105eff68c64fa8b1dab7ff3fefd68bced2
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-05-15 19:12:38 -0700

    Added widgets directly to properties to have them Bulma stylized

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,15 +5,25 @@ from django.contrib.auth.models import User
 from django import forms
 import datetime
 
+from django.forms.widgets import Widget
 
+
+# Creating this class to be able to add additional fields to the Sign Up form in signup.html
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    current_city = forms.CharField(max_length=50)
-    current_country = forms.CharField(max_length=50)
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs= {'class': 'input is-success'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs= {'class': 'input is-success'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs= {'class': 'input is-success'}))
+    current_city = forms.CharField(max_length=50, widget=forms.TextInput(attrs= {'class': 'input is-success'}))
+    current_country = forms.CharField(max_length=50, widget=forms.TextInput(attrs= {'class': 'input is-success'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs= {'class': 'input is-success'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs= {'class': 'input is-success'}))
     date_joined  =datetime.datetime.now()
 
     class Meta():
         model = User
         fields = ('username', 'first_name', 'last_name')
+
+        # Widgets are to create styling on the sign up form
+        # Styling is from Bulma form styling documentation https://bulma.io/documentation/form/general/
+
     
