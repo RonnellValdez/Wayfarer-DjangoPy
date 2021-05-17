cacc49103d800b7cd1a71ab1c4fa803f4ed8ce89
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-05-17 08:48:16 -0700

    merge

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,15 +1,4 @@
 console.log("connected");
-// import bulmaCarousel from '~bulma-carousel/dist/js/bulma-carousel.min.js';
-
-// // Initialize all elements with carousel class.
-// const carousels = bulmaCarousel.attach('.carousel', options);
-
-// // To access to bulmaCarousel instance of an element
-// const element = document.querySelector('#my-element');
-// if (element && element.bulmaCarousel) {
-// 	// bulmaCarousel instance is available as element.bulmaCarousel
-// };
-
 console.log("working");
 // To activate the hamburger menu in the navabar
 $(".navbar-burger").click(function () {
@@ -18,19 +7,27 @@ $(".navbar-burger").click(function () {
 	$(".navbar-menu").toggleClass("is-active");
 });
 
-
-$(".modal-button").click(function() {
-	let target = $(this).data("target");
-	$("html").addClass("is-clipped");
-	$(target).addClass("is-active");
+document.getElementById('log-in-tag').addEventListener('click', function() {
+	document.querySelector('.bg-modal').style.display = "flex";
 });
 
-$(".modal-close").click(function() {
-	$("html").removeClass("is-clipped");
-	$(this).parent().removeClass("is-active");
+document.querySelector('.close').addEventListener('click', function() {
+	document.querySelector('.bg-modal').style.display = "none";
 });
 
-let slideIndex = 1;
+// $(".modal-button").click(function() {
+// 	let target = $(this).data("target");
+// 	$("html").addClass("is-clipped");
+// 	$(target).addClass("is-active");
+// });
+
+// $(".modal-close").click(function() {
+// 	$("html").removeClass("is-clipped");
+// 	$(this).parent().removeClass("is-active");
+// });
+
+
+var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -38,8 +35,23 @@ function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-
 // Thumbnail image controls
 function currentSlide(n) {
-	showSlides(slideIndex = n);
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
