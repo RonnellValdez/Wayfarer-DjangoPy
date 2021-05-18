499b9d9c2e66a608cac52d1a2acda0edc6726b2b
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-05-18 10:48:14 -0700

    merge

--- main_app/static/scripts/main.js
+++ main_app/static/scripts/main.js
@@ -1,16 +1,4 @@
 
-// import bulmaCarousel from '~bulma-carousel/dist/js/bulma-carousel.min.js';
-
-// // Initialize all elements with carousel class.
-// const carousels = bulmaCarousel.attach('.carousel', options);
-
-// // To access to bulmaCarousel instance of an element
-// const element = document.querySelector('#my-element');
-// if (element && element.bulmaCarousel) {
-// 	// bulmaCarousel instance is available as element.bulmaCarousel
-// }
-
-
 // To activate the hamburger menu in the navabar
 $(".navbar-burger").click(function () {
 	// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
